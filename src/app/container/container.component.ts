a1e816ef3bfa3669435d70ad6f65e7317276ead5
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-08 17:35:51 +0530

    day1 session2 part1

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+})
+export class ContainerComponent implements OnInit {
+  price = 100.25;
+  title = 'Hello friends!';
+  isActive = false;
+  today = new Date();
+  pi = 3.14159265358979323846;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
