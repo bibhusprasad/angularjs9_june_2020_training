ad701d0edfe7bba47eb257de1ddf8a35f66f544f
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-09 10:59:47 +0530

    day2 session1 part2 table added

--- src/app/current-items/current-items.component.ts
+++ src/app/current-items/current-items.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoItem, todoList } from 'src/_data/data';
 
 @Component({
   selector: 'app-current-items',
   templateUrl: './current-items.component.html',
-  styleUrls: ['./current-items.component.scss']
+  styleUrls: ['./current-items.component.scss'],
 })
 export class CurrentItemsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  currentItems(): ToDoItem[] {
+    return todoList;
   }
-
 }
