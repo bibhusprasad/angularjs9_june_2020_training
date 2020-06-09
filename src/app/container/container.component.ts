29750dd767a3563354d42bb6fa57a311a9c72f57
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-09 08:47:24 +0530

    day2 session1 part1 input property declaration

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-container',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent implements OnInit {
+  @Input() currentScore: number;
+
   constructor() {}
+
   ngOnInit(): void {}
 }
