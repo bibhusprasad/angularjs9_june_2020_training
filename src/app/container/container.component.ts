e0787df62bcab218d6f5c2e0d39840fc890d4df8
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-09 09:01:21 +0530

    day2 session1 part1 input property declaration1

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
   @Input() currentScore: number;
+  @Input() today: Date;
 
   constructor() {}
 
