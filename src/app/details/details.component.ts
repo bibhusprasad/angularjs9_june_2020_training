ba6d0742302fd3fe733576e3d9a601d7ddd84dfb
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-10 17:24:49 +0530

    day3 session2 part1 router corrected1

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  constructor(private ar: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.id = +this.ar.snapshot.paramMap.get('id');
+    if (!this.id) {
+      console.log('Oops');
+    } else {
+      console.log('Good', this.id);
+    }
   }
-
 }
