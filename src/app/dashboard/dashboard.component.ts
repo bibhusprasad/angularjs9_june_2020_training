2ee283c77a27e47051106f48a3d687ab3cb844e4
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2020-06-10 18:22:21 +0530

    day3 ss2 part1 statastics dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AppStatistics } from 'src/_data/data';
+import { TodoService } from '../_services/todo.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  statistics: AppStatistics;
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.statistics = this.todoService.getStatistics();
   }
-
 }
